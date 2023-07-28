dfa1c63426000ce5a8a8ab9dd285110e7ae16381
Erika Ferreira <erikarocha.salvatore@gmail.com>
2023-07-28 17:08:47 -0300

    update main
    Função entrar valores na tela

--- main.py
+++ main.py
@@ -24,81 +24,95 @@ frame_cientifica.grid(row=1, column=0)
 frame_corpo = Frame(janela, width=300, height=340) 
 frame_corpo.grid(row=2, column=0)
 
+# Funções de  Valores
+global todos_valores
+
+todos_valores = ''
+texto=StringVar()
+
+# Função entrar valores na tela
+def entrar_valores(evento):
+    global todos_valores
+    
+    todos_valores = todos_valores + str(evento)
+    texto.set(todos_valores)
+
+
 # Configurando o frame tela
 
-Label_tela = Label(frame_tela, text='123456789' ,width=16, height=2,padx=7, anchor='e',bd=0, justify=RIGHT, font=('Ivy 18'),bg=cor3, fg=cor2)
+Label_tela = Label(frame_tela, textvariable=texto, width=16, height=2,padx=7, anchor='e',bd=0, justify=RIGHT, font=('Ivy 18'),bg=cor3, fg=cor2)
 Label_tela.place(x=0, y=0)
 
 # Configurando o frame cientifica
-b_0 = Button(frame_cientifica, text='tan', width=6, height=1, relief=RAISED, overrelief="ridge",font=('Ivy 10 bold'), bg=cor1, fg=cor2)
+b_0 = Button(frame_cientifica, command=lambda:entrar_valores('tan'), text='tan', width=6, height=1, relief=RAISED, overrelief="ridge",font=('Ivy 10 bold'), bg=cor1, fg=cor2)
 b_0.place(x=0, y=0)
-b_1 = Button(frame_cientifica, text='sin', width=6, height=1, relief=RAISED, overrelief="ridge",font=('Ivy 10 bold'), bg=cor1, fg=cor2)
+b_1 = Button(frame_cientifica, command=lambda:entrar_valores('sin'), text='sin', width=6, height=1, relief=RAISED, overrelief="ridge",font=('Ivy 10 bold'), bg=cor1, fg=cor2)
 b_1.place(x=59, y=0)
-b_2 = Button(frame_cientifica, text='cos', width=6, height=1, relief=RAISED, overrelief="ridge",font=('Ivy 10 bold'), bg=cor1, fg=cor2)
+b_2 = Button(frame_cientifica, command=lambda:entrar_valores('cos'), text='cos', width=6, height=1, relief=RAISED, overrelief="ridge",font=('Ivy 10 bold'), bg=cor1, fg=cor2)
 b_2.place(x=118, y=0)
-b_3 = Button(frame_cientifica, text='sqrt', width=6, height=1, relief=RAISED, overrelief="ridge",font=('Ivy 10 bold'), bg=cor1, fg=cor2)
+b_3 = Button(frame_cientifica, command=lambda:entrar_valores('sqrt'), text='sqrt', width=6, height=1, relief=RAISED, overrelief="ridge",font=('Ivy 10 bold'), bg=cor1, fg=cor2)
 b_3.place(x=177, y=0)
 
-b_0 = Button(frame_cientifica, text='log', width=6, height=1, relief=RAISED, overrelief="ridge",font=('Ivy 10 bold'), bg=cor1, fg=cor2)
+b_0 = Button(frame_cientifica, command=lambda:entrar_valores('log'), text='log', width=6, height=1, relief=RAISED, overrelief="ridge",font=('Ivy 10 bold'), bg=cor1, fg=cor2)
 b_0.place(x=0, y=29)
-b_1 = Button(frame_cientifica, text='log10', width=6, height=1, relief=RAISED, overrelief="ridge",font=('Ivy 10 bold'), bg=cor1, fg=cor2)
+b_1 = Button(frame_cientifica, command=lambda:entrar_valores('log10'), text='log10', width=6, height=1, relief=RAISED, overrelief="ridge",font=('Ivy 10 bold'), bg=cor1, fg=cor2)
 b_1.place(x=59, y=29)
-b_2 = Button(frame_cientifica, text='e', width=6, height=1, relief=RAISED, overrelief="ridge",font=('Ivy 10 bold'), bg=cor1, fg=cor2)
+b_2 = Button(frame_cientifica, command=lambda:entrar_valores('e'), text='e', width=6, height=1, relief=RAISED, overrelief="ridge",font=('Ivy 10 bold'), bg=cor1, fg=cor2)
 b_2.place(x=118, y=29)
-b_3 = Button(frame_cientifica, text='pow', width=6, height=1, relief=RAISED, overrelief="ridge",font=('Ivy 10 bold'), bg=cor1, fg=cor2)
+b_3 = Button(frame_cientifica, command=lambda:entrar_valores('pow'), text='pow', width=6, height=1, relief=RAISED, overrelief="ridge",font=('Ivy 10 bold'), bg=cor1, fg=cor2)
 b_3.place(x=177, y=29)
 
-b_0 = Button(frame_cientifica, text='pi', width=6, height=1, relief=RAISED, overrelief="ridge",font=('Ivy 10 bold'), bg=cor1, fg=cor2)
+b_0 = Button(frame_cientifica, command=lambda:entrar_valores('pi'),  text='pi', width=6, height=1, relief=RAISED, overrelief="ridge",font=('Ivy 10 bold'), bg=cor1, fg=cor2)
 b_0.place(x=0, y=58)
-b_1 = Button(frame_cientifica, text=',', width=6, height=1, relief=RAISED, overrelief="ridge",font=('Ivy 10 bold'), bg=cor1, fg=cor2)
+b_1 = Button(frame_cientifica, command=lambda:entrar_valores(','), text=',', width=6, height=1, relief=RAISED, overrelief="ridge",font=('Ivy 10 bold'), bg=cor1, fg=cor2)
 b_1.place(x=59, y=58)
-b_2 = Button(frame_cientifica, text='(', width=6, height=1, relief=RAISED, overrelief="ridge",font=('Ivy 10 bold'), bg=cor1, fg=cor2)
+b_2 = Button(frame_cientifica, command=lambda:entrar_valores('('), text='(', width=6, height=1, relief=RAISED, overrelief="ridge",font=('Ivy 10 bold'), bg=cor1, fg=cor2)
 b_2.place(x=118, y=58)
-b_3 = Button(frame_cientifica, text=')', width=6, height=1, relief=RAISED, overrelief="ridge",font=('Ivy 10 bold'), bg=cor1, fg=cor2)
+b_3 = Button(frame_cientifica, command=lambda:entrar_valores(')'), text=')', width=6, height=1, relief=RAISED, overrelief="ridge",font=('Ivy 10 bold'), bg=cor1, fg=cor2)
 b_3.place(x=177, y=58)
 
 # Frame corpo
 
-b_0 = Button(frame_corpo, text='C', width=14, height=1, relief=RAISED, overrelief="ridge",font=('Ivy 10 bold'), bg=cor4, fg=cor2)
+b_0 = Button(frame_corpo, command=lambda:entrar_valores('C'), text='C', width=14, height=1, relief=RAISED, overrelief="ridge",font=('Ivy 10 bold'), bg=cor4, fg=cor2)
 b_0.place(x=0, y=0)
-b_1 = Button(frame_corpo, text='%', width=6, height=1, relief=RAISED, overrelief="ridge",font=('Ivy 10 bold'), bg=cor4, fg=cor2)
+b_1 = Button(frame_corpo, command=lambda:entrar_valores('%'), text='%', width=6, height=1, relief=RAISED, overrelief="ridge",font=('Ivy 10 bold'), bg=cor4, fg=cor2)
 b_1.place(x=118, y=0)
-b_2 = Button(frame_corpo, text='/', width=6, height=1, relief=RAISED, overrelief="ridge",font=('Ivy 10 bold'), bg=cor4, fg=cor2)
+b_2 = Button(frame_corpo, command=lambda:entrar_valores('/'), text='/', width=6, height=1, relief=RAISED, overrelief="ridge",font=('Ivy 10 bold'), bg=cor4, fg=cor2)
 b_2.place(x=177, y=0)
 
           
-b_0 = Button(frame_corpo, text='7', width=6, height=1, relief=RAISED, overrelief="ridge",font=('Ivy 10 bold'), bg=cor1, fg=cor2)
+b_0 = Button(frame_corpo, command=lambda:entrar_valores('7'), text='7', width=6, height=1, relief=RAISED, overrelief="ridge",font=('Ivy 10 bold'), bg=cor1, fg=cor2)
 b_0.place(x=0, y=29)
-b_1 = Button(frame_corpo, text='8', width=6, height=1, relief=RAISED, overrelief="ridge",font=('Ivy 10 bold'), bg=cor1, fg=cor2)
+b_1 = Button(frame_corpo, command=lambda:entrar_valores('8'), text='8', width=6, height=1, relief=RAISED, overrelief="ridge",font=('Ivy 10 bold'), bg=cor1, fg=cor2)
 b_1.place(x=59, y=29)
-b_2 = Button(frame_corpo, text='9', width=6, height=1, relief=RAISED, overrelief="ridge",font=('Ivy 10 bold'), bg=cor1, fg=cor2)
+b_2 = Button(frame_corpo, command=lambda:entrar_valores('9'), text='9', width=6, height=1, relief=RAISED, overrelief="ridge",font=('Ivy 10 bold'), bg=cor1, fg=cor2)
 b_2.place(x=118, y=29)
-b_3 = Button(frame_corpo, text='*', width=6, height=1, relief=RAISED, overrelief="ridge",font=('Ivy 10 bold'), bg=cor4, fg=cor2)
+b_3 = Button(frame_corpo, command=lambda:entrar_valores('*'), text='*', width=6, height=1, relief=RAISED, overrelief="ridge",font=('Ivy 10 bold'), bg=cor4, fg=cor2)
 b_3.place(x=177, y=29)
 
-b_0 = Button(frame_corpo, text='4', width=6, height=1, relief=RAISED, overrelief="ridge",font=('Ivy 10 bold'), bg=cor1, fg=cor2)
+b_0 = Button(frame_corpo, command=lambda:entrar_valores('4'), text='4', width=6, height=1, relief=RAISED, overrelief="ridge",font=('Ivy 10 bold'), bg=cor1, fg=cor2)
 b_0.place(x=0, y=58)
-b_1 = Button(frame_corpo, text='5', width=6, height=1, relief=RAISED, overrelief="ridge",font=('Ivy 10 bold'), bg=cor1, fg=cor2)
+b_1 = Button(frame_corpo, command=lambda:entrar_valores('5'), text='5', width=6, height=1, relief=RAISED, overrelief="ridge",font=('Ivy 10 bold'), bg=cor1, fg=cor2)
 b_1.place(x=59, y=58)
-b_2 = Button(frame_corpo, text='6', width=6, height=1, relief=RAISED, overrelief="ridge",font=('Ivy 10 bold'), bg=cor1, fg=cor2)
+b_2 = Button(frame_corpo, command=lambda:entrar_valores('6'), text='6', width=6, height=1, relief=RAISED, overrelief="ridge",font=('Ivy 10 bold'), bg=cor1, fg=cor2)
 b_2.place(x=118, y=58)
-b_3 = Button(frame_corpo, text='-', width=6, height=1, relief=RAISED, overrelief="ridge",font=('Ivy 10 bold'), bg=cor4, fg=cor2)
+b_3 = Button(frame_corpo, command=lambda:entrar_valores('-'), text='-', width=6, height=1, relief=RAISED, overrelief="ridge",font=('Ivy 10 bold'), bg=cor4, fg=cor2)
 b_3.place(x=177, y=58)
 
-b_0 = Button(frame_corpo, text='1', width=6, height=1, relief=RAISED, overrelief="ridge",font=('Ivy 10 bold'), bg=cor1, fg=cor2)
+b_0 = Button(frame_corpo, command=lambda:entrar_valores('1'), text='1', width=6, height=1, relief=RAISED, overrelief="ridge",font=('Ivy 10 bold'), bg=cor1, fg=cor2)
 b_0.place(x=0, y=87)
-b_1 = Button(frame_corpo, text='2', width=6, height=1, relief=RAISED, overrelief="ridge",font=('Ivy 10 bold'), bg=cor1, fg=cor2)
+b_1 = Button(frame_corpo, command=lambda:entrar_valores('2'), text='2', width=6, height=1, relief=RAISED, overrelief="ridge",font=('Ivy 10 bold'), bg=cor1, fg=cor2)
 b_1.place(x=59, y=87)
-b_2 = Button(frame_corpo, text='3', width=6, height=1, relief=RAISED, overrelief="ridge",font=('Ivy 10 bold'), bg=cor1, fg=cor2)
+b_2 = Button(frame_corpo, command=lambda:entrar_valores('3'), text='3', width=6, height=1, relief=RAISED, overrelief="ridge",font=('Ivy 10 bold'), bg=cor1, fg=cor2)
 b_2.place(x=118, y=87)
-b_3 = Button(frame_corpo, text='+', width=6, height=1, relief=RAISED, overrelief="ridge",font=('Ivy 10 bold'), bg=cor4, fg=cor2)
+b_3 = Button(frame_corpo, command=lambda:entrar_valores('+'), text='+', width=6, height=1, relief=RAISED, overrelief="ridge",font=('Ivy 10 bold'), bg=cor4, fg=cor2)
 b_3.place(x=177, y=87)
           
-b_0 = Button(frame_corpo, text='0', width=14, height=1, relief=RAISED, overrelief="ridge",font=('Ivy 10 bold'), bg=cor1, fg=cor2)
+b_0 = Button(frame_corpo, command=lambda:entrar_valores('0'), text='0', width=14, height=1, relief=RAISED, overrelief="ridge",font=('Ivy 10 bold'), bg=cor1, fg=cor2)
 b_0.place(x=0, y=116)
-b_1 = Button(frame_corpo, text='.', width=6, height=1, relief=RAISED, overrelief="ridge",font=('Ivy 10 bold'), bg=cor1, fg=cor2)
+b_1 = Button(frame_corpo, command=lambda:entrar_valores('.'), text='.', width=6, height=1, relief=RAISED, overrelief="ridge",font=('Ivy 10 bold'), bg=cor1, fg=cor2)
 b_1.place(x=118, y=116)
-b_2 = Button(frame_corpo, text='=', width=6, height=1, relief=RAISED, overrelief="ridge",font=('Ivy 10 bold'), bg=cor4, fg=cor2)
+b_2 = Button(frame_corpo, command=lambda:entrar_valores('='), text='=', width=6, height=1, relief=RAISED, overrelief="ridge",font=('Ivy 10 bold'), bg=cor4, fg=cor2)
 b_2.place(x=177, y=116)
 
 
